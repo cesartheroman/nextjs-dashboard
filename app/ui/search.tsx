3e07d8cd4d8714f2ae8464f490fa59a634a4cc44
Cesar Roman <cesardroman18@gmail.com>
2024-04-25 13:53:43 -0500

    feat: implemented pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,10 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    //TODO: Debouncing
-    console.log(`Searching.. ${term}`);
-
     const params = new URLSearchParams();
+    params.set('page', "1")
 
     if (term) {
       params.set('query', term);
